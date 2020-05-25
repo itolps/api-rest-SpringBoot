de8bc12bcbee5e47b338acdda2002f0a5c807f26
italolopes9 <it-alo19@hotmail.com>
2020-05-25 17:44:17 -0300

    first commit

---
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -0,0 +1,12 @@
+package com.produtos.apirest.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_PRODUTO")
+public class Produto implements Serializable {
+
+}
