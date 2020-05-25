0c9d76b0b6b37550cd1a0cb9b30b91dd9d7124fb
italolopes9 <it-alo19@hotmail.com>
2020-05-25 20:54:07 -0300

    Criado método deletar Produtos

--- src/main/java/com/produtos/apirest/controllers/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controllers/ProdutoController.java
@@ -4,6 +4,7 @@ package com.produtos.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,11 @@ public class ProdutoController {
 	@PostMapping("/produto")
 	public Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
 	}
 	
 }
