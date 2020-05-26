cc8ce6db07a3125fd63251618a2dd4f5c5fcd7ce
italolopes9 <it-alo19@hotmail.com>
2020-05-25 21:02:10 -0300

    ok

--- src/main/java/com/produtos/apirest/controllers/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controllers/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,8 +46,10 @@ public class ProdutoController {
 	}
 	
 	@DeleteMapping("/produto/{id}")
-	public void deletaProduto(@PathVariable(value="id")long id) {
+	public void deletaProdutoId(@PathVariable(value="id")long id) {
 		produtoRepository.deleteById(id);
 	}
+	
+	
 	
 }
